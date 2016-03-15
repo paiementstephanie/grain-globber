678e290c6ccd3a41d3403e40953b0d50e69ce8b3
paiementstephanie <stephanie.paiement@rogers.com>
2016-03-15 14:07:45 -0400

    added omg attachment

--- js/main.js
+++ js/main.js
@@ -8,3 +8,9 @@ grains.forEach(function (grain) {
   var $p = $('<p>');
 
   $h2.html(grain.name);
+  $img.attr('src', 'images/' + grain.img);
+  $p.html(grain.desc);
+
+  $li.append($img, $h2, $p);
+  $ul.append($li);
+});
